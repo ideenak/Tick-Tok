480e6734d99e8d7cb5f1c8a3b487968f4a6a2c74
ideenak <45948963+ideenak@users.noreply.github.com>
2019-04-30 14:11:37 -0400

    Create my_script.py

---
+++ my_script.py
@@ -0,0 +1,36 @@
+
+
+
+def enlarge(i):
+    if(message_count == 0):
+        date = "xyz"
+        notification = "Tick-Tok Portfolio Management \nDate: " + date + "\nNone of your stocks had price movements that exceeded the threshold."
+
+        message = client.messages \
+                        .create(
+                            body=notification,
+                            #from_='+15405180462',
+                            #to=recipient
+                        )
+        return message
+
+
+def notificationsigma(i):
+    message_count = 0
+    notification = "Tick-Tok Portfolio Management \n"
+
+    pctchg = 100
+    symbol = "AGR"
+    #this value was replaced for running the pytest (not the actual value)
+
+
+
+
+    if ((pctchg*100) > threshold or (pctchg*100) < (threshold * -1)):
+                        message_count += 1
+                        output_pct_chg = "{0:,.2f}".format(pctchg*100)
+                        notification += "\n" + f"     {symbol} moved {output_pct_chg}% today."
+                        return notificationsigma
+
+
+#certain variables that were irrelevant to the actual test were changed
